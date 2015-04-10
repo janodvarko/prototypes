92fa9e317f7f560f99b67591ca2b3f830fc73741
Jan Odvarko <odvarko@gmail.com>
2015-04-10 14:27:27 +0200

    Remove obsolete libs

--- json-viewer/data/config.js
+++ json-viewer/data/config.js
@@ -7,7 +7,6 @@ require.config({
     "jquery": "lib/jquery/jquery",
     "react": "lib/react/react",
     "bootstrap": "lib/bootstrap/js/bootstrap",
-    "immutable": "lib/immutable/immutable",
     "react-bootstrap": "lib/react-bootstrap/react-bootstrap",
     "reps": "../node_modules/firebug.sdk/lib/reps",
   }
