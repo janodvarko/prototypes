9008b3050a8bc642fc68147da088e55d69987f75
Jan Odvarko <odvarko@gmail.com>
2016-06-21 09:03:16 +0200

    Fix sidebar.html test

--- sidebar.html/index.js
+++ sidebar.html/index.js
@@ -2,11 +2,50 @@
 
 "use strict";
 
-var Sidebar = React.createFactory(require("devtools/client/shared/components/tabs/sidebar"));
+window.trace = function(message, obj) {
+  console.log(message, obj)
+}
 
-// Render the Sidebar.
+var Tabbar = React.createFactory(require("devtools/client/shared/components/tabs/tabbar"));
+var SidePanel = React.createFactory(require("devtools/client/inspector/components/side-panel"));
+
+// Render the Tabbar
 let content = document.getElementById("content");
-let sidebar = ReactDOM.render(Sidebar({}), content);
+let tabbar = ReactDOM.render(Tabbar({}), content);
 
-sidebar.addTab("tab1", "tab1.html");
-sidebar.addTab("tab2", "tab2.html", true);
+// Shortcuts
+const { div, input, button, label, span } = React.DOM;
+
+var TestPanel = React.createFactory(React.createClass({
+  displayName: "TestPanel",
+
+  componentDidMount: function () {
+    console.log('componentDidMount ' + this.props.id);
+  },
+
+  componentWillUnmount: function () {
+    console.log('componentWillUnmount ' + this.props.id);
+  },
+
+  render: function () {
+    let boxStyle = {
+      overflow: 'auto',
+      height: '100%',
+    }
+
+    let style = {
+      height: '2500px',
+    };
+
+    return (
+      div({style: boxStyle},
+        input({type: "checkbox", id: "checkbox"}),
+        span({}, "check box"),
+        div({style: style}, "this should scroll xxx")
+      )
+    )
+  }
+}));
+
+tabbar.addTab("tab1", "tab1.html", false, TestPanel);
+tabbar.addTab("tab2", "tab2.html", false, TestPanel);
